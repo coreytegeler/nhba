ed7e316e6c2d13611df5075f43833132f8e5da2e
coreytegeler <coreytegeler@gmail.com>
2016-11-08 13:36:50 -0500

    backup

--- config/vars.js
+++ config/vars.js
@@ -5,7 +5,8 @@ module.exports = {
   	test: 80
   },
   db: {
-	  production: 'mongodb://heroku_9lc8j8h8:3p9j6i3d186egmg131vfqrjgb7@ds029735.mlab.com:29735/heroku_9lc8j8h8',
+    production: 'localhost:27017/new-haven-building-archive',
+	  // production: 'mongodb://heroku_9lc8j8h8:3p9j6i3d186egmg131vfqrjgb7@ds029735.mlab.com:29735/heroku_9lc8j8h8',
 	  development: 'localhost:27017/new-haven-building-archive',
 	  test: 'localhost:27017/new-haven-building-archive'
 	}
